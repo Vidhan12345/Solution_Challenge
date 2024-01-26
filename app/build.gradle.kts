dd5534db03bc430e679b62239eb41ffd1fdbd3a7
Vidhan <vidhandahatkar@gmail.com>
2024-01-26 12:35:59 +0530

    new repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.google.android.gms:play-services-location:21.0.1")
 
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.squareup.picasso:picasso:2.8")
 
 }
