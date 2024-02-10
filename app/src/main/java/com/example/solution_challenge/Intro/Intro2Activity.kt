b346471b0a9effc19c35159bbd250053c09ec6ba
Vidhan <vidhandahatkar@gmail.com>
2024-02-10 11:36:28 +0530

    background changed

--- app/src/main/java/com/example/solution_challenge/Intro/Intro2Activity.kt
+++ app/src/main/java/com/example/solution_challenge/Intro/Intro2Activity.kt
@@ -15,7 +15,7 @@ class Intro2Activity : AppCompatActivity() {
 
         val start = findViewById<Button>(R.id.startBt)
         start.setOnClickListener {
-            val  intent = Intent(this,SignUpActivity::class.java)
+            val  intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
